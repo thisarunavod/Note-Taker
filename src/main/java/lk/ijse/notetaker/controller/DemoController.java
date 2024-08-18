6685f04df3f5ed3d97ec1877cf27ca1ce39c28d2
thisaru <amithawerahena@gmail.com>
2024-08-18 12:59:06 +0530

    first commit

---
+++ src/main/java/lk/ijse/notetaker/controller/DemoController.java
@@ -0,0 +1,12 @@
+package lk.ijse.notetaker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/demo")
+@RestController
+public class DemoController {
+
+
+}
